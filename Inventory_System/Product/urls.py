21e313c488c581439dbdbba12b613afc3b265efd
Akindipe Muheez <akindipemuheez@gmail.com>
2025-01-03 11:37:31 +0100

    Added end point for inventory levels

--- Inventory_System/Product/urls.py
+++ Inventory_System/Product/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, CategoryViewSet, SupplierViewSet
+
+
 router = DefaultRouter()
 
 # Create a router and register the ViewSets with it
@@ -8,7 +10,9 @@ router.register(r'products', ProductViewSet, basename='product')
 router.register(r'category', CategoryViewSet, basename='category')
 router.register(r'suppliers', SupplierViewSet, basename='supplier')
 
-# Include the router's URL patterns
+# URL patterns
 urlpatterns = [
     path('', include(router.urls)),  # Automatically generates URL patterns for CRUD actions
+    path('products/levels', ProductViewSet.as_view({'get': 'current_inventory'}), name='current_inventory'),
 ]
+
